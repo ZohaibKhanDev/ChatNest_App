07b865613aaa7042a84ccbe18a1e8c45f1f2284c
Zohaib Khan <zohaibkhan@192.168.10.4>
2024-04-19 04:59:27 -0700

    chatdetail

--- app/src/main/java/com/example/chatnest/presentation/ui/screen/ChatDetail.kt
+++ app/src/main/java/com/example/chatnest/presentation/ui/screen/ChatDetail.kt
@@ -2,31 +2,49 @@ package com.example.chatnest.presentation.ui.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.outlined.CameraAlt
+import androidx.compose.material.icons.outlined.KeyboardVoice
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerDefaults
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -56,7 +74,7 @@ fun ChatDetail(navController: NavController, image: String?, name: String?) {
                     )
 
                     AsyncImage(
-                        model =image , contentDescription = "", modifier = Modifier
+                        model = image, contentDescription = "", modifier = Modifier
                             .width(36.dp)
                             .height(36.dp)
                     )
@@ -84,13 +102,81 @@ fun ChatDetail(navController: NavController, image: String?, name: String?) {
                     )
                 })
         }, bottomBar = {
-            BottomAppBar {
-                TextField(value = message, onValueChange = { message = it }, leadingIcon = {
-                    Image(painter = painterResource(id = R.drawable.document), contentDescription = "")
-                }, modifier = Modifier.width(228.dp).height(32.dp), shape = RoundedCornerShape(1f))
+            BottomAppBar(modifier = Modifier.fillMaxWidth(), containerColor = Color.White) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "",
+                        tint = Color(0XFF007AFF), modifier = Modifier.clickable { }
+                    )
+                    TextField(
+                        value = message,
+                        onValueChange = { message = it },
+                        trailingIcon = {
+                            Image(
+                                painter = painterResource(id = R.drawable.document),
+                                contentDescription = "", modifier = Modifier
+                                    .width(18.dp)
+                                    .height(19.dp)
+                                    .clickable { }, contentScale = ContentScale.Crop
+                            )
+                        },
+                        modifier = Modifier
+                            .width(228.dp)
+                            .height(50.dp),
+                        shape = RoundedCornerShape(20.dp),
+                        colors = TextFieldDefaults.textFieldColors(
+                            focusedTextColor = Color.Gray,
+                            unfocusedTextColor = Color.Gray,
+                            focusedIndicatorColor = Color.White,
+                            unfocusedIndicatorColor = Color.White,
+                            focusedLeadingIconColor = Color.Blue,
+                            unfocusedLeadingIconColor = Color.Blue
+                        ),
+                        textStyle = TextStyle(
+                            fontSize = 18.sp,
+                            textAlign = TextAlign.Start,
+                            textDecoration = TextDecoration.LineThrough,
+                            textMotion = TextMotion.Animated
+                        )
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.outlinecamera),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(22.dp)
+                            .height(19.dp)
+                            .clickable { },
+                        contentScale = ContentScale.FillBounds
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.audio),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .width(16.dp)
+                            .height(24.dp)
+                            .clickable { }
+                    )
+                }
             }
         }
     ) {
 
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Image(
+                painter = painterResource(id = R.drawable.wallpaper),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+
+            )
+        }
     }
+
 }
+
